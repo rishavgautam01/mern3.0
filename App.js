8dbfc74ff48a6f0793b2bddc0f855cfa67a4e514
Rishav Gautam <bishnudawadi909@gmail.com>
2024-10-03 13:25:04 +0545

    new api

--- App.js
+++ App.js
@@ -6,6 +6,11 @@ app.get("/",(req,res)=>{
         message:"This is a home page"
     })
 })
+app.get("/contact",(req,res)=>{
+    res.json({
+        message:"This is a contact page"
+    })
+})
 
 
 app.listen(3000,()=>{
